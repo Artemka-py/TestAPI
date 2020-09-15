eb36c3d1a0e91e71a030d107fd6c8e64a123ee4c
Artemka-py <djartlyt@gmail.com>
2020-09-16 00:30:22 +0300

    fixed

--- nodeAPI/app.js
+++ nodeAPI/app.js
@@ -4,14 +4,14 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/api/:text', async (req, res) => {
+app.post('/api/:text', (req, res) => {
   if (!req.params.text) return res.sendStatus(400)
   return res.send({ text: req.params.text })
 })
 
 const PORT = 5000
 
-async function start() {
+function start() {
   try {
     app.listen(PORT, () => console.log(`App has been started in port ${PORT}`))
   } catch (e) {
