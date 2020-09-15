ae064ae5181f27abd1d74505dfd5cf7aaff5cb24
Artemka-py <djartlyt@gmail.com>
2020-09-16 00:27:44 +0300

    Bug fixed

--- GolangAPI/main.go
+++ GolangAPI/main.go
@@ -1,18 +1,12 @@
 package main
 
-import (
-"net/http"
-
-	"github.com/gin-gonic/gin"
-
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
 	r.POST("/api/:text", func(c *gin.Context) {
-		text := c.Param("text")
-		c.JSON(http.StatusOK, map[string]string{
-			"text": text,
+		c.JSON(200, gin.H{
+			"text": c.Param("text"),
 		})
 	})
 	r.Run()
--- nodeAPI/app.js
+++ nodeAPI/app.js
@@ -4,14 +4,14 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/api', async (req, res) => {
-  if (!req.body) return res.sendStatus(400)
-  return res.send({ text: req.query.text })
+app.post('/api/:text', async (req, res) => {
+  if (!req.params.text) return res.sendStatus(400)
+  return res.send({ text: req.params.text })
 })
 
 const PORT = 5000
 
-function start() {
+async function start() {
   try {
     app.listen(PORT, () => console.log(`App has been started in port ${PORT}`))
   } catch (e) {
